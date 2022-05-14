d119921fa0ca05cb401482eb0a29b5563c2a56ea
Cyril <cyril.russe@hotmail.com>
2022-05-14 11:08:16 +0200

    correction merge main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+
+#include "view.hpp"
+#include "controller.hpp"
+#include "projectile.hpp"
+
+#define NBR_OF_FPS 30
+
+int main(){
+    View view(NBR_OF_FPS);
+    Controller controller;
+    
+    while (controller.inGame()){
+        view.setBackground();
+
+        controller.handleInputs();
+
+        view.show();
+    }
+
+    view.closeView();
+
+
+    return 0;
+}
