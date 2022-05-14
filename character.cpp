de3305552ffd23ac1b2e96f43307e0208d880053
Cyril <cyril.russe@hotmail.com>
2022-05-14 11:07:42 +0200

    subclass character init

--- character.cpp
+++ character.cpp
@@ -1,5 +1,47 @@
 #include "character.hpp"
 
-Character::Character(){
-    
+Character::Character(int x, int y){
+    position_x = x;
+    position_y = y;
+}
+
+Wolf::Wolf():Character(WOLF_INIT_POS){
+    switch ((rand()%5)+1){
+    case 1:
+        falling_pos = WOLF_FALLING_POS1;
+        break;
+    case 2:
+        falling_pos = WOLF_FALLING_POS2;
+        break;
+    case 3:
+        falling_pos = WOLF_FALLING_POS3;
+        break;
+    case 4:
+        falling_pos = WOLF_FALLING_POS4;
+        break;
+    case 5:
+        falling_pos = WOLF_FALLING_POS5;
+        break;
+    }
+    status = WALKING;
+}
+
+MamaPig::MamaPig(SDL_Renderer *rend) : Character(MAMAPIG_INIT_POS)
+{
+    // mama_pig_texture = SDL_CreateTextureFromSurface(rend, SDL_LoadBMP("../sprites/PigMom.bmp"));
+    // mama_pig_image_dimension = {5, 5, 20, 10};
+    ;
+}
+
+SDL_Texture *MamaPig::showMamaPig(SDL_Renderer *rend){
+    // mama_pig_texture = SDL_CreateTextureFromSurface(rend, SDL_LoadBMP("../sprites/PigMom.bmp"));
+    // mama_pig_image_dimension = {5, 5, 20, 10};
+    // SDL_RenderCopy(rend, mama_pig_texture, NULL, &mama_pig_image_dimension);
+
+    SDL_Surface *surf_test = SDL_LoadBMP("../sprites/Arrow.bmp");
+    SDL_Texture *text_test = SDL_CreateTextureFromSurface(rend, surf_test);
+    // SDL_Rect image_rect = {5, 5, 20, 10};
+    // SDL_RenderCopy(rend, text_test, NULL, &image_rect);
+
+    return text_test;
 }
--- character.hpp
+++ character.hpp
@@ -1,38 +1,63 @@
 #ifndef _CHARACTER_HPP
 #define _CHARACTER_HPP
 
-enum wolf_status
-{
+#include <cstdlib>
+#include <SDL2/SDL.h>
+
+#include "projectile.hpp"
+
+#define WOLF_INIT_POS 50, 82
+#define WOLF_VELOCITY 50, 0
+#define WOLF_FALLING_VELOCITY 0, 50
+#define WOLD_FREE_FALLING_VELOCITY 0, 100
+#define WOLF_FALLING_POS1 75
+#define WOLF_FALLING_POS2 125
+#define WOLF_FALLING_POS3 150
+#define WOLF_FALLING_POS4 200
+#define WOLF_FALLING_POS5 250
+
+#define MAMAPIG_INIT_POS 400, 240
+#define MAMAPIG_BOUND_MIN 170
+#define MAMAPIG_BOUND_MAX 405
+
+
+
+enum wolf_status{
     WALKING,
     BALLON_FALL,
     FREE_FALLING
 };
 
-class Character
-{
+class Character{
 private:
-    int x, y;
-    //Projectiles?
+    int position_x, position_y;
 public:
-    Character();
-    ~Character();
+    Character(int x, int y);
 };
 
-class Wolf : public Character
-{
+class Wolf : public Character{
 private:
     wolf_status status;
-
+    int falling_pos;
 public:
+    //Constructor
+    Wolf();
     //Access
     wolf_status getStatus(){return status;}
 };
 
-class MamaPig : public Character
-{
+class MamaPig : public Character{
 private:
-
+    Arrow *loaded_arrow;
+    SDL_Texture *mama_pig_texture;
+    SDL_Rect mama_pig_image_dimension;
 public:
+    //Constructor
+    MamaPig(SDL_Renderer *rend);
+
+    //Access
+    
+    SDL_Texture *showMamaPig(SDL_Renderer *rend);
 };
 
 #endif
